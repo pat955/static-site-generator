abbcc85933cfba1a2c86717241af5164f3836c6d
pat955 <patricijap8@gmail.com>
2024-03-08 13:01:27 +0100

    regex recognises multiple brackets

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -18,24 +18,34 @@ class TestSplitNodes(unittest.TestCase):
 
         self.assertEqual(res, answer)
     
-    # def test_split_images2(self):
-    #     text = TextNode("text test text test image here soon!! ![nope] here: ![image](https://example.png)  !!!! second image???? ![2](https://hello.png)", "text")
-    #     res = split_nodes_image([text])
-    #     answer = [
-    #     TextNode("text test text test image here soon!! ![nope] here: ", "text"),
-    #     TextNode("image", "image", "https://example.png"),
-    #     TextNode("  !!!! second image???? ", "text"),
-    #     TextNode("2", "image", "https://hello.png")
-    #     ]
-    #     self.assertEqual(res, answer)
+    def test_split_images2(self):
+        node = TextNode("text test text test image here soon!! ![nope] here: ![image](https://example.png)  !!!! second image???? ![2](https://hello.png)", "text")
+        res = split_nodes_image([node])
+        answer = [
+        TextNode("text test text test image here soon!! ![nope] here: ", "text"),
+        TextNode("image", "image", "https://example.png"),
+        TextNode("  !!!! second image???? ", "text"),
+        TextNode("2", "image", "https://hello.png")
+        ]
+        self.assertEqual(res, answer)
     
-    # def test_split_links(self):
-    #     text = TextNode("text test text test image here soon!! ![nope] here: ![link!](https://example.png)  !!!! second image???? ![2](https://hello.png)", "text")
-    #     res = split_nodes_links([text])
-    #     answer = [
-    #     TextNode("text test text test image here soon!! ![nope] here: ", "text"),
-    #     TextNode("link!", "link", "https://example.png"),
-    #     TextNode("  !!!! second image???? ", "text"),
-    #     TextNode("2", "image", "https://hello.png")
-    #     ]
-    #     self.assertEqual(res, answer)
+    def test_split_links(self):
+        node = TextNode("text test text test image here soon!! ![nope] here: ![link!](https://example.png)  !!!! second image???? ![2](https://hello.png)", "text")
+        res = split_nodes_links([node])
+        answer = [
+        TextNode("text test text test image here soon!! ![nope] here: !", "text"),
+        TextNode("link!", "link", "https://example.png"),
+        TextNode("  !!!! second image???? !", "text"),
+        TextNode("2", "link", "https://hello.png")
+        ]
+        self.assertEqual(res, answer)
+    
+    def test_split_links2(self):
+        node = TextNode("Hello John! ![a trick] [link](https://no.com)... [fakelink]]((https::///))", "text")
+        res = split_nodes_links([node])
+        answer = [
+        TextNode("Hello John! ![a trick] ", "text"),
+        TextNode("link", "link", "https://no.com"),
+        TextNode("... [fakelink]]((https::///))", "text"),
+        ]
+        self.assertEqual(res, answer)
